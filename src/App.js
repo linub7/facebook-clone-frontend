7656036f0cded661d1a79e4232d2449450feb56f
a1a2a3a4a5 <linub7@gmail.com>
2022-04-17 10:44:34 +0430

    Implement login profile home routes and make absolute path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Home from 'pages/home';
+import Login from 'pages/login';
+import Profile from 'pages/profile';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <div>welcome to frontend</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} exact />
+        <Route path="/profile" element={<Profile />} exact />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "./styles/icons/icons.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './styles/icons/icons.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </Router>,
+  document.getElementById('root')
 );
